207dfa5df60b6ed6c18b710b4a3fc74a0ab4e08e
Joezeppe <46751967+Joezeppe@users.noreply.github.com>
2020-05-19 02:21:58 -0700

    Update write_json_infile.py

--- write_json_infile.py
+++ write_json_infile.py
@@ -1,3 +1,2 @@
-with open("{}/Cloud_DA/PyQUBO4FDAU/notebook_files/{}".format(my_machine_path, file_to_execute),
-                      "w+") as nf:
-                json.dump(notebook_data, nf)
+with open("f.json","w+") as nf:
+  json.dump(notebook_data, nf)
